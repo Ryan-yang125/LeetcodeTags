a36cc23fdd446d844f7a8f8345fe49dee2c93ea8
Ryan-yang125 <1598094295@qq.com>
2020-12-10 00:09:36 +0800

    update

--- DP/L516.js
+++ DP/L516.js
@@ -3,16 +3,16 @@
  * @return {number}
  */
 var longestPalindromeSubseq = function (s) {
-  let m = s.length;
-  let dp = new Array(m);
-  for (let i = 0; i < m; i++) {
-    dp[i] = new Array(m).fill(0);
-    dp[i][i] = 1;
-  }
-  for (let i = m - 2; i >= 0; i--)
-    for (let j = i + 1; j < m; j++) {
+  let n = s.length;
+  if (!n || n === 1) return n;
+  let dp = Array(n)
+    .fill(null)
+    .map(() => Array(n).fill(0));
+  for (let i = 0; i < n; i++) dp[i][i] = 1;
+  for (let i = n - 2; i >= 0; i--)
+    for (let j = i + 1; j < n; j++) {
       if (s[i] === s[j]) dp[i][j] = dp[i + 1][j - 1] + 2;
       else dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
     }
-  return dp[0][m - 1];
+  return dp[0][n - 1];
 };
--- DP/L53.js
+++ DP/L53.js
@@ -3,15 +3,11 @@
  * @return {number}
  */
 var maxSubArray = function (nums) {
-  if (!nums.length) return 0;
-  //base
-  let dp0 = nums[0];
-  let dp1 = 0;
-  let res = dp0;
+  if (!nums) return;
+  let res = (prev = nums[0]);
   for (let i = 1; i < nums.length; i++) {
-    dp1 = Math.max(nums[i], nums[i] + dp0);
-    dp0 = dp1;
-    res = Math.max(res, dp1);
+    prev = Math.max(nums[i], prev + nums[i]);
+    res = Math.max(res, prev);
   }
   return res;
 };
